69a6fc16b60a4eeaad6a8d0321daa3a55b380680
ALEFG <alessioforgione42@gmail.com>
2024-12-22 18:45:27 +0100

    File Main aggiornato. Aggiunto connessione al db

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
 
+        // Carica le informazioni di configurazione dal file config.properties (bisogna creare il file config.properties)
+        Properties properties = new Properties();
+        try (FileInputStream input = new FileInputStream("config.properties")) {
+            properties.load(input);
+        } catch (IOException ex) {
+            System.out.println("Sorry, unable to find config.properties");
+            ex.printStackTrace();
+            return;
+        }
+        // URL del database
+        String url = properties.getProperty("db.url");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Credenziali di accesso al database
+        String username = properties.getProperty("db.username");
+        String password = properties.getProperty("db.password");
+
+        // Connessione al database
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            if (connection != null) {
+                System.out.println("Connessione al database avvenuta con successo!");
+            }
+        } catch (SQLException e) {
+            System.out.println("Errore nella connessione al database");
+            e.printStackTrace();
         }
     }
 }
