e3fdddb2230ae50488208ef57fa8574cbda4c495
ALEFG <alessioforgione42@gmail.com>
2024-12-27 17:41:58 +0100

    Implementazione del Metodo Iscrivi

--- src/Fruitrice.java
+++ src/Fruitrice.java
@@ -1,7 +1,28 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Fruitrice extends Azienda {
     public Fruitrice(String partita_iva, TipoAzienda tipo, String mission, String nome, int numeroDipendenti) {
         super(partita_iva, tipo, mission, nome, numeroDipendenti);
     }
 
-    public void iscrivi(Classe classe, int numeroDipendenti) {}
+    public void iscrivi(int codice_classe, int numeroDipendenti) {
+        String query = "INSERT INTO Iscrizione (id_azienda, codice_classe, n_dipendenti)" +
+                "VALUES (?, ?, ?)";
+
+        try(Connection conn = Database.getConnection();
+            PreparedStatement ps = conn.prepareStatement(query)) {
+
+            ps.setString(1, this.getPartita_iva());
+            ps.setInt(2, codice_classe);
+            ps.setInt(3, numeroDipendenti);
+
+            ps.executeUpdate();
+            System.out.println("Iscrizione avvenuta");
+
+        } catch(SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
